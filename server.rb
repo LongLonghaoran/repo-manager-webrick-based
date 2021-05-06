82a565a83841cf0acfbabe1daf7350804b2311a4
eddy <18621781488@163.com>
2021-05-07 00:36:09 +0800

    add log

--- server.rb
+++ server.rb
@@ -15,32 +15,33 @@ require 'rack'
 require 'json'
 require 'open3'
 dir = File.expand_path 'uploads'
+stdout,stderr,status = ["", "", ""]
+logger_file = File.new("log.txt", "w+")
 app = proc do |env|
-    puts "request comming: ---------------------------------------------------------------------------"
-    puts "#{Time.now}"
+    logger_file.puts "request comming: ---------------------------------------------------------------------------"
+    logger_file.puts "#{Time.now}"
     req = Rack::Request.new(env)
-    puts env
+    logger_file.puts env
     file = req.params['file']
     full_path = req.fullpath
     File.open("#{dir}/#{file[:filename]}", 'w+') do |f|
         f.write file[:tempfile].read
     end
-    stdout,stderr,status = ["", "", ""]
     # run sh
     case full_path
     when /repos\/api\/products\/V10/
-        puts "executing ./deb-partner-v10.sh #{dir}/#{file[:filename]}:\n"
+        logger_file.puts "executing ./deb-partner-v10.sh #{dir}/#{file[:filename]}:\n"
         stdout,stderr,status = Open3.capture3("./deb-partner-v10.sh #{dir}/#{file[:filename]}")
-        puts "stdout:#{stdout}"
-        puts "stderr:#{stderr}"
-        puts "status:#{status}"
+        logger_file.puts "stdout:#{stdout}"
+        logger_file.puts "stderr:#{stderr}"
+        logger_file.puts "status:#{status}"
     when /repos\/api\/products\/V10-arm64/
-        puts "pwd: \n" + `pwd`
-        puts "executing ./deb-desktop-v10-arm64.sh #{dir}/#{file[:filename]}:\n"
+        logger_file.puts "pwd: \n" + `pwd`
+        logger_file.puts "executing ./deb-desktop-v10-arm64.sh #{dir}/#{file[:filename]}:\n"
         stdout,stderr,status = Open3.capture3("./deb-desktop-v10-arm64.sh #{dir}/#{file[:filename]}")
-        puts "stdout:#{stdout}"
-        puts "stderr:#{stderr}"
-        puts "status:#{status}"
+        logger_file.puts "stdout:#{stdout}"
+        logger_file.puts "stderr:#{stderr}"
+        logger_file.puts "status:#{status}"
     else
         raise "wrong repo name"
     end
